609da7e4765d3470c7bdd7fa75f9d7c8084c8bb0
Sheika1404 <rosheika1404@gmail.com>
2021-10-18 08:27:59 -0400

    react reaturant init

---
+++ react-pizza/src/components/Products/data.js
@@ -0,0 +1,70 @@
+import product1 from "../../images/product-1.jpg";
+import product2 from "../../images/product-2.jpg";
+import product3 from "../../images/product-3.jpg";
+import Brownie from "../../images/brownie.jpg";
+import StrawberryIceCream from "../../images/strawberry-ice-cream.jpg";
+import Donuts from "../../images/doughnuts.jpg";
+import ChocolateIceCream from "../../images/chocolate-Ice-cream.jpg";
+
+export const productData = [
+  {
+    img: product1,
+    alt: "Pizza",
+    name: "Suprime Pizza",
+    desc: "Marina sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
+    price: "$19.99",
+    button: "Add to Cart",
+  },
+  {
+    img: product2,
+    alt: "Pizza",
+    name: "Hawaiian Paradise",
+    desc: "Marina sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
+    price: "$16.99",
+    button: "Add to Cart",
+  },
+  {
+    img: product3,
+    alt: "Donuts",
+    name: "Veggie Overload",
+    desc: "Marina sauce, basil, italian sausage, roma tomatoes, olives, and pesto",
+    price: "$14.99",
+    button: "Add to Cart",
+  },
+];
+
+
+export const productData2 = [
+  {
+    img: Brownie,
+    alt: "Brownie",
+    name: "Molten Chocolate Brownie",
+    desc: "Chocolate Brownie Square with Fudge Chocolate Icing and Chocolate Sauce",
+    price: "$19.99",
+    button: "Add to Cart",
+  },
+  {
+    img: StrawberryIceCream,
+    alt: "Strawberry Ice Cream",
+    name: "Strawberry Delight",
+    desc: "Vanilla Ice Cream topped with Strawberries and strawberry Sauce",
+    price: "$16.99",
+    button: "Add to Cart",
+  },
+  {
+    img: Donuts,
+    alt: "Donuts ",
+    name: "Classic Donuts ",
+    desc: "Crispy Fried Donouts sprinkled with confection sugar",
+    price: "$14.99",
+    button: "Add to Cart",
+  },
+  {
+    img: ChocolateIceCream,
+    alt: "Chocolate Ice Cream",
+    name: "Chocolate Dream",
+    desc: "Chocolate Brownie with a scoope of Chocolate Ice Cream covered with chocolate sauce",
+    price: "$14.99",
+    button: "Add to Cart",
+  }
+];
